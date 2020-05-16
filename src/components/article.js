5264e3704c76bbfb95871ae22a1857403a579954
alessio <alegue87@gmail.com>
2020-05-16 18:41:12 +0200

    AceEditor + parser ( da finire )

--- src/components/article.js
+++ src/components/article.js
@@ -1,28 +1,56 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { Container, Loader } from 'semantic-ui-react'
 import { resetPosts } from '../actions'
+import AceEditor  from 'react-ace';
+import parse, { domToReact } from 'html-react-parser'
 
 
 class Article extends React.Component {
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.props.resetPosts()
   }
-  componentDidMount(){
+  componentDidMount() {
     this.props.resetPosts()
   }
-  
+
+  hasClass(name, list) {
+    let res = false;
+    list.forEach(v => {
+      if (v === name) {
+        res = true;
+      }
+    })
+    return res
+  }
+
+
   render() {
+
     //setTimeout(()=>{jQuery('.wp-block-simple-code-block-ace').removeAttr('style')},100)
     let content;
+    let html = this.props.children
+
+    const options = {
+      replace: domNode => {
+        if (domNode.attribs && domNode.attribs.class) {
+          const classes = domNode.attribs.class
+          if (classes.split(' ').find(c => c === 'wp-block-simple-code-block-ace')) {
+            console.log(domNode.children[0].children[0].data)
+            return <AceEditor value={domNode.children[0].children[0].data}/>
+          }
+        }
+      }
+    }
     if (this.props.children !== undefined) {
-      content = <Container text dangerouslySetInnerHTML={{ __html: this.props.children }} />
+      content = <Container text >{
+        parse(html, options)}</Container>
     }
     else {
-      content = <Container style={{minHight:'100px'}}>
-        <Loader active/>
+      content = <Container style={{ minHight: '100px' }}>
+        <Loader active />
       </Container>
     }
     return (
@@ -31,7 +59,7 @@ class Article extends React.Component {
   }
 }
 
-function mapDispatchToProps(dispatch){
-  return bindActionCreators({resetPosts}, dispatch)
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ resetPosts }, dispatch)
 }
 export default connect(null, mapDispatchToProps)(Article);
